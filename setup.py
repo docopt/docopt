38b5eb4ea454710dbc51a2014d34856e5aa1f786
Vladimir Keleshev <halst@ya.ru>
2012-04-13 17:25:46 +0200

    Rename README back, small changes to setup.py.

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-import os
+""" `docopt` lives on `GitHub <http://github.com/halst/docopt/>`_"""
 from setuptools import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name = "docopt",
-    version = "0.1",
+    version = "0.1.1",
     author = "Vladimir Keleshev",
     author_email = "vladimir@keleshev.com",
     description = "Pythonic option parser, that will make you smile",
     license = "MIT",
     keywords = "option parsing optparse argparse getopt",
-    url = "https://github.com/halst/docopt",
+    url = "http://github.com/halst/docopt",
     py_modules=['docopt'],
-    long_description=read('README'),
+    long_description=__doc__,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
