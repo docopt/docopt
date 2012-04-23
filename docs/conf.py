5ee8b82117df3b72b31d93e60bd40a1e4551cfa8
Vladimir Keleshev <halst@ya.ru>
2012-04-23 19:38:36 +0200

    Change Sphinx theme.

--- docs/conf.py
+++ docs/conf.py
@@ -91,12 +91,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'  #'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+#html_theme_options = {'nosidebar':'true'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
