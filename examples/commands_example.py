dccff4caceaa6d5ab9739c091ad1d9afd235868a
Vladimir Keleshev <halst@ya.ru>
2012-06-04 18:23:22 +0200

    Add more fun to commands_example.py.

--- examples/commands_example.py
+++ examples/commands_example.py
@@ -1,14 +1,19 @@
 """Example of program with many commands.
 
 Usage:
-  commands_example.py ship move (up|down|left|right) [--fast]
+  commands_example.py ship new <name>...
+  commands_example.py ship [<name>] move (up|down|left|right) [--speed=<kn>]
   commands_example.py ship shoot <x> <y>
-  commands_example.py mine (set|remove) <x> <y>
-  commands_example.py (-h | --help)
+  commands_example.py mine (set|remove) <x> <y> [--moored|--drifting]
+  commands_example.py -h | --help
+  commands_example.py --version
 
 Options:
-  -h --help  Show help screen.
-  --fast     Move ship *fast*.
+  -h --help     Show help screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots
+  --moored      Mored (anchored) mine.
+  --drifting    Drifting mine.
 
 """
 from docopt import docopt
