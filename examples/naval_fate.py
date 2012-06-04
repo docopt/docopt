8b4389093ec03d48d53d4a27916117914eb91b75
Vladimir Keleshev <halst@ya.ru>
2012-06-04 20:51:54 +0200

    Rename commands_example.py -> naval_fate.py.

---
+++ examples/naval_fate.py
@@ -0,0 +1,24 @@
+"""Naval Fate.
+
+Usage:
+  naval_fate.py ship new <name>...
+  naval_fate.py ship [<name>] move <x> <y> [--speed=<kn>]
+  naval_fate.py ship shoot <x> <y>
+  naval_fate.py mine (set|remove) <x> <y> [--moored|--drifting]
+  naval_fate.py -h | --help
+  naval_fate.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Mored (anchored) mine.
+  --drifting    Drifting mine.
+
+"""
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Naval Fate 2.0')
+    print(arguments)
