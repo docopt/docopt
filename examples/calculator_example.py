f7e4642993c2620a624bb0c776e2c49c2d41f859
Vladimir Keleshev <halst@ya.ru>
2012-06-12 19:15:55 +0200

    Allow minus in calculator example.

--- examples/calculator_example.py
+++ examples/calculator_example.py
@@ -1,13 +1,13 @@
 """Not a serious example.
 
 Usage:
-  calculator_example.py <value> ( ( + | * | ^ | / ) <value> )...
+  calculator_example.py <value> ( ( + | - | * | / ) <value> )...
   calculator_example.py <function> <value> [( , <value> )]...
   calculator_example.py (-h | --help)
 
 Examples:
   calculator_example.py 1 + 2 + 3 + 4 + 5
-  calculator_example.py 1 + 2 '*' 3 / 4 ^ 5    # note quotes around '*'
+  calculator_example.py 1 + 2 '*' 3 / 4 - 5    # note quotes around '*'
   calculator_example.py sum 10 , 20 , 30 , 40
 
 Options:
