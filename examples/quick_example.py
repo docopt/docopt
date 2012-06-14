4fa601011c5e5f566b52a836de24f795e8fa43dc
Vladimir Keleshev <halst@ya.ru>
2012-06-14 18:40:45 +0200

    Example without option descriptions.

---
+++ examples/quick_example.py
@@ -0,0 +1,12 @@
+"""Usage:
+  quick_example.py tcp <host> <port> [--timeout=<seconds>]
+  quick_example.py serial <port> [--baud=9600] [--timeout=<seconds>]
+  quick_example.py -h | --help | --version
+
+"""
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1.1rc')
+    print(arguments)
