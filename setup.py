4036d9a5cc6a3250c84c6484756c876ea2bcd558
Andrew Kassen <atkassen@ucdavis.edu>
2012-06-20 12:43:23 -0700

    add list classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,11 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
         "License :: OSI Approved :: MIT License",
     ],
 )
